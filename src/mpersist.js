b711043ad1fb0b85f734140b3267316ba09a4175
Guy Bianco IV <gjbiancoiv@gmail.com>
2019-05-26 13:39:33 -0400

    fully recursive

--- src/mpersist.js
+++ src/mpersist.js
@@ -1,11 +1,9 @@
 mpersist=p=n=>n<10?0:p(s(n))+1
-s=(n,a=1)=>{
-  while(n>10){
-    a*=n%10
-    n=Math.floor(n/10)
-  }
-  return a
-}
+s=(n,a=1)=>n==0?1:n%10*s(n/10|0)
+
+// above only works up to 9007199254740991
+// this should work up to 1.7976931348623157e+308 :
+// s=(n,a=1)=>n==0?1:n%10*s(Math.trunc(n/10))
 
 
 /* test harness */
@@ -15,9 +13,8 @@ let tests = [
   [ 123, 1],
   [5499, 2],
   [277777788888899, 11],
-  // [77777733332222222222222222222, 11]
+  // [77777733332222222222222222222, 11] // TOO LARGE!
 ]
-console.clear()
 for(let t of tests) {
   let r = mpersist(t[0])
   assert(r === t[1], `got ${r} instead of ${t[1]}`)
